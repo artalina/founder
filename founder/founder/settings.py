375c21e3dcde3838efccc61a9143e423b572fa48
Алекс Артем <sandronator1975@gmail.com>
2020-09-14 00:34:20 +0300

    db

--- founder/founder/settings.py
+++ founder/founder/settings.py
@@ -78,13 +78,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'founder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'lnu',
+        }
     }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
