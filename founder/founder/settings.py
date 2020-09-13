ad00c89ac1da17644b98c0107d3ec48d4d7e52b1
Алекс Артем <sandronator1975@gmail.com>
2020-09-14 00:39:54 +0300

    Revert "setting STATICFILES"
    This reverts commit 2c5b497d4d9ec1686ae5f638cd1cd31b0ced5920.

--- founder/founder/settings.py
+++ founder/founder/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
