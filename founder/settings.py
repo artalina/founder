67c5f5adb79a2acf5ebff1988068192752c70a50
Алекс Артем <sandronator1975@gmail.com>
2020-09-19 14:55:49 +0300

    req django_on_heroku

--- founder/settings.py
+++ founder/settings.py
@@ -14,6 +14,7 @@ import os
 import posixpath
 import dj_database_url
 import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,4 +129,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
+
+
+django_on_heroku.settings(locals())
 
